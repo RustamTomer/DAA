0a8c06fe03ffa0cee50af56bd0f3761db7558edf
Rustam Singh Tomer <91615721+RustamTomer@users.noreply.github.com>
2023-01-27 08:59:36 +0530

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,7 +4,7 @@ void bubbleSortExample(int arr[], int n){
         int Swap = 0;    
         for (int j = 0; j < n - i - 1; ++j){    
             if (arr[j] > arr[j + 1]){
-                int temp = arr[x];
+                int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;      
                 Swap = 1;
